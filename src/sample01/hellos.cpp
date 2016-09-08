30af95224a2e2cd0803d32424652ff7e8ae6ad04
liyiwei <git@liyiwei.org>
2016-09-08 17:40:20 +0800

    multi-line string

---
+++ src/sample01/hellos.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // c++ multi-line/part string 
+    // http://en.cppreference.com/w/cpp/language/string_literal
+    // not a good style in my personal opinion though
+    cout << "this is a " 
+        "test with multiple strings" << endl;
+
+    return 0;
+}
